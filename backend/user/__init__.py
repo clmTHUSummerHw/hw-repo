1eede1fc264d3bdbe57d775ddf7647de93a5c299
Chlamydomonos <2457242458@qq.com>
2022-07-09 16:44:53 +0800

    Fixed problems in backend

--- backend/user/__init__.py
+++ backend/user/__init__.py
@@ -1,18 +1,17 @@
-from crypt import methods
 from flask import Blueprint
-from register import register
-from login import login
-from logout import logout
-from list_projects import list_projects
-from new_project import new_project
-from delete_project import delete_project
+from user.register import register
+from user.login import login
+from user.logout import logout
+from user.list_projects import list_projects
+from user.new_project import new_project
+from user.delete_project import delete_project
 
 
 user_api = Blueprint('user', __name__)
 
-user_api.add_url_rule('/', 'register', register, methods=['POST'])
-user_api.add_url_rule('/', 'login', login, methods=['POST'])
-user_api.add_url_rule('/', 'logout', logout, methods=['POST'])
-user_api.add_url_rule('/', 'list-projects', list_projects, methods=['POST'])
-user_api.add_url_rule('/', 'new-project', new_project, methods=['POST'])
-user_api.add_url_rule('/', 'delete-project', delete_project, methods=['POST'])
+user_api.add_url_rule('/register', view_func=register, methods=['POST'])
+user_api.add_url_rule('/login', view_func=login, methods=['POST'])
+user_api.add_url_rule('/logout', view_func=logout, methods=['POST'])
+user_api.add_url_rule('/list-projects', view_func=list_projects, methods=['POST'])
+user_api.add_url_rule('/new-project', view_func=new_project, methods=['POST'])
+user_api.add_url_rule('/delete-project', view_func=delete_project, methods=['POST'])
