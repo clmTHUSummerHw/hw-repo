f3be2a5b92d9b3f7fc9a3fdc8a779d7839fd6448
Chlamydomonos <2457242458@qq.com>
2022-08-17 12:22:10 +0800

    Added some css

--- frontend/src/components/MonacoEditor.vue
+++ frontend/src/components/MonacoEditor.vue
@@ -23,3 +23,16 @@ export default defineComponent({
     }
 })
 </script>
+
+<style lang="scss">
+.editor-container
+{
+    width: 100%;
+    height: 100%;
+
+    *
+    {
+        text-align: left;
+    }
+}
+</style>
