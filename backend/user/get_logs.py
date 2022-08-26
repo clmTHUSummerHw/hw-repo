634d9c2460b95760f5fe8fbbc2a217bfa3414075
Chlamydomonos <2457242458@qq.com>
2022-08-26 21:24:49 +0800

    sort by time

--- backend/user/get_logs.py
+++ backend/user/get_logs.py
@@ -1,6 +1,6 @@
 import re
 from flask import request, jsonify
-from db.models import User
+from db.models import User, Log
 from db import db
 
 def get_logs():
@@ -23,11 +23,11 @@ def get_logs():
         return jsonify({'code': 2, 'log': []})
 
     logs = []
-    for log in project.log.all():
+    for log in project.log.order_by(Log.time.desc()).all():
         alone = {}
         alone['code'] = log.code
         alone['time'] = log.time
         alone['extra_data'] = log.extra_data
         logs.append(alone)
 
-    return jsonify({'code': 0, 'log': logs}) # 返回0（返回项目日志） 
+    return jsonify({'code': 0, 'log': logs}) # 返回0（返回项目日志）
