c381fa07d9f95b2c3dfa86f991e614988d3bf049
Macchiatobin <chanbin0502@naver.com>
2022-08-26 12:24:43 +0800

    modified:   backend/run/input.py 	modified:   backend/run/start.py

--- backend/run/input.py
+++ backend/run/input.py
@@ -1,2 +1,11 @@
-def input(data): #TODO: 获取输入，并传递给对应用户的项目 / data: session, projectname, text()
-    pass
+from run.running_users import running_users
+
+
+def input(data): #TODO: 获取输入，并传递给对应用户的项目 / data: session, project, text
+
+    session = data.session
+    project = data.project #用不到？
+    inputText = data.text
+
+    currentThread = running_users[session]
+    currentThread.communicate(inputText)
