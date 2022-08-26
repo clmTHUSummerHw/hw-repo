22b125f3e2da46fc8248e7d33a7c9e717830c853
Chlamydomonos <2457242458@qq.com>
2022-08-26 19:43:21 +0800

    Completed things

--- backend/project/remove_dependency.py
+++ backend/project/remove_dependency.py
@@ -1,2 +1,25 @@
+import os
+from flask import request, jsonify
+from project.utils import get_dependency_root
+
+
 def remove_dependency():
-    pass
+    if not request.is_json:
+        return jsonify({'code': -1})
+
+    session = request.json['session']
+    project_name = request.json['project']
+    dependency_name = request.json['dependency']
+
+    if not isinstance(session, str) or not isinstance(project_name, str) or not isinstance(dependency_name, str):
+        return jsonify({'code': -1})
+
+    code, root = get_dependency_root(session, project_name)
+    if code != 0:
+        return jsonify({'code': code})
+
+    try:
+        os.remove(root + '/' + dependency_name)
+        return jsonify({'code': 0})
+    except Exception:
+        return jsonify({'code': 3})
---
+++ frontend/src/utils/post-util/ListDependenciesResult.ts
@@ -0,0 +1,5 @@
+export default class ListDependenciesResult
+{
+    code = -1
+    dependencies: string[] = []
+}
